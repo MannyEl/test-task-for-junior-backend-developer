d2e6b509bda1e4e8bac557b3d50434d5e50f18ce
Eldar <manny2002@mail.ru>
2026-04-09 20:11:26 +0500

    a bit refactoring

--- internal/transport/http/handlers/dto.go
+++ internal/transport/http/handlers/dto.go
@@ -6,6 +6,22 @@ import (
 	taskdomain "example.com/taskservice/internal/domain/task"
 )
 
+type EvenOdd string
+
+const (
+	Even EvenOdd = "even"
+	Odd  EvenOdd = "odd"
+)
+
+type Settings struct {
+	StartDate     time.Time   `json:"start_date"`
+	EndDate       time.Time   `json:"end_date"`
+	IntervalDays  int         `json:"interval_days"`
+	MonthDays     []int       `json:"month_days"`
+	SpecificDates []time.Time `json:"specific_dates"`
+	EvenOdd       EvenOdd     `json:"even_odd"`
+}
+
 type taskMutationDTO struct {
 	Title       string            `json:"title"`
 	Description string            `json:"description"`
@@ -19,6 +35,7 @@ type taskDTO struct {
 	Status      taskdomain.Status `json:"status"`
 	CreatedAt   time.Time         `json:"created_at"`
 	UpdatedAt   time.Time         `json:"updated_at"`
+	Settings
 }
 
 func newTaskDTO(task *taskdomain.Task) taskDTO {
--- internal/transport/http/handlers/task_handler.go
+++ internal/transport/http/handlers/task_handler.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -130,17 +129,6 @@ func getIDFromRequest(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func decodeJSON(r *http.Request, dst any) error {
-	decoder := json.NewDecoder(r.Body)
-	decoder.DisallowUnknownFields()
-
-	if err := decoder.Decode(dst); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func writeUsecaseError(w http.ResponseWriter, err error) {
 	switch {
 	case errors.Is(err, taskdomain.ErrNotFound):
@@ -150,17 +138,4 @@ func writeUsecaseError(w http.ResponseWriter, err error) {
 	default:
 		writeError(w, http.StatusInternalServerError, err)
 	}
-}
-
-func writeError(w http.ResponseWriter, status int, err error) {
-	writeJSON(w, status, map[string]string{
-		"error": err.Error(),
-	})
-}
-
-func writeJSON(w http.ResponseWriter, status int, payload any) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-
-	_ = json.NewEncoder(w).Encode(payload)
 }
